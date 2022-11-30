22f907d64c2077305c6a4bd329aef48a0a2c852c
houssam almomani <houssamalmomani1@gmail.com>
2022-11-30 16:46:53 +0300

    working on HTTP Req

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
-import { Route, useParams } from 'react-router-dom';
+import { Link, Route, useParams, useRouteMatch } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 const DUMMY_QUOTES = [
 	{ id: 'q1', author: 'Houssam', text: 'Learning React' },
 	{ id: 'q2', author: 'Houssam', text: 'finishing this course' },
 ];
+
 const QuoteDetail = () => {
+	const match = useRouteMatch();
 	const params = useParams();
 
 	const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
@@ -20,7 +22,20 @@ const QuoteDetail = () => {
 				text={quote.text}
 				author={quote.author}
 			/>
-			<Route path={`/quotes/${params.quoteId}/comments`}>
+			<Route
+				path={match.path}
+				exact
+			>
+				<div className='centered'>
+					<Link
+						className='btn--flat'
+						to={`${match.url}/comments`}
+					>
+						Load Comments
+					</Link>
+				</div>
+			</Route>
+			<Route path={`${match.path}/comments`}>
 				<Comments />
 			</Route>
 		</Fragment>
